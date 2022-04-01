8d044aca43a760a24188a3fb92266f337323cf71
Arthit Suriyawongkul <arthit@gmail.com>
2022-04-01 10:22:29 +0100

    Check if directory exist

--- pdf-to-txt.sh
+++ pdf-to-txt.sh
@@ -6,13 +6,20 @@ IFS=','
 INPUT_LIST=filenames.csv
 INPUT_DIR=data/nat-ai/orig
 OUTPUT_DIR=data/nat-ai/text
+PDFBOX_JAR=lib/pdfbox-app-3.jar
 
 echo "Extract text from PDF files"
-echo "Input list (CSV): $INPUT_LIST"
-echo "Input directory:  $INPUT_DIR"
-echo "Output directory: $OUTPUT_DIR"
+echo "PDFBox jar file:   $PDFBOX_JAR"
+echo "Input list (CSV):  $INPUT_LIST"
+echo "Input directory:   $INPUT_DIR"
+echo "Output directory:  $OUTPUT_DIR"
+echo ""
 
+[ ! -f $PDFBOX_JAR ] && { echo "$PDFBOX_JAR file not found"; exit 99; }
 [ ! -f $INPUT_LIST ] && { echo "$INPUT_LIST file not found"; exit 99; }
+[ ! -d $INPUT_DIR ] && { echo "$INPUT_DIR directory not found"; exit 99; }
+[ ! -d $OUTPUT_DIR ] && { echo "$OUTPUT_DIR directory not found"; exit 99; }
+
 while read Country CountryCode Language Year Title Filename URL AIWatchURL
 do
     if [[ $Filename == *.pdf ]]
@@ -20,7 +27,8 @@ do
         OUTPUT="$Year-$CountryCode-$Language.txt"
         echo "Input : $Filename"
         echo "Output: $OUTPUT"
-        java -jar lib/pdfbox-app-3.jar export:text --input=$INPUT_DIR/$Filename --output=$OUTPUT_DIR/$OUTPUT
+        echo ""
+        java -jar $PDFBOX_JAR export:text --input=$INPUT_DIR/$Filename --output=$OUTPUT_DIR/$OUTPUT
     fi
 done < $INPUT_LIST
 
